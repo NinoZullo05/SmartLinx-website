2d29a28e3d2434d092c9bd98123863eccec0958d
Nino Zullo <nino.zullo05dev@gmail.com>
2024-05-24 09:33:50 +0200

    updated APKs, added apiStatus link and fixed various bug

--- src/App.js
+++ src/App.js
@@ -8,33 +8,16 @@ import Download from './Components/Download';
 import FAQ from './Components/FAQ';
 import FAQGrid from './Components/FAQGrid';
 import Footer from './Components/Footer';
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 function App () {
   return (
     <div className="bg-bg_light dark:bg-bg_dark">
       <NavBar />
       <Home />
-<<<<<<< HEAD
-
-      <Services />
-
-      <Feature />
-
-      <Download />
-
-      <FAQ />
-      <FAQGrid />
-
-=======
       <Services />
       <Feature />
       <Download />
       <FAQ />
       <FAQGrid />
->>>>>>> master
       <Footer />
     </div>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,6 @@ import React, { useState } from "react";
 import { SlArrowRight, SlArrowDown } from "react-icons/sl";
 import { FaRegCopyright, FaGithub } from "react-icons/fa";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 function Footer() {
     const [showDetails, setShowDetails] = useState({
         Anes: false,
@@ -94,106 +90,85 @@ function Footer() {
                             Useful Links
                         </h6>
                         <p className="mb-4">
-<<<<<<< HEAD
-                            <a href="https://github.com/SmartLinx-Project" className="text-neutral-600 dark:text-neutral-200" target="_blank" // To open the repository in another page
-                            >
-=======
                             <a href="https://github.com/SmartLinx-Project" className="text-neutral-600 dark:text-neutral-200" target="_blank" rel="noopener noreferrer">
->>>>>>> master
-            Source Code
+                                Source Code
 
-        </a>
-    </p>
-        <p className="mb-4">
-            <a href="Documentation/ProjectInfo.pdf"
-                className="text-neutral-600 dark:text-neutral-200"
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-            >
-                Project Information
-            </a>
-        </p>
-        <p className="mb-4">
-<<<<<<< HEAD
-    <button
-        className="text-neutral-600 dark:text-neutral-200 hover:text-green-500"
-        onMouseOver={(e) => e.currentTarget.textContent = 'Online'}
-        onMouseOut={(e) => e.currentTarget.textContent = 'API Status'}>
-        API Status
-    </button>
-=======
+                            </a>
+                        </p>
+                        <p className="mb-4">
+                            <a href="Documentation/ProjectInfo.pdf"
+                                className="text-neutral-600 dark:text-neutral-200"
+                            >
+                                Project Information
+                            </a>
+                        </p>
+                        <p className="mb-4">
                             <a href="https://stats.uptimerobot.com/cOQ8m3hFKF"
                                 target="_blank" rel="noopener noreferrer"
                                 className="text-neutral-600 dark:text-neutral-200 hover:text-green-500">
                                 API Status
                             </a>
->>>>>>> master
                         </p >
-        <p>
-            <a href="Documentation/SmartLinx_API Reference.pdf"
-                className="text-neutral-600 dark:text-neutral-200"
-            >
-                API Documentation
-            </a>
-        </p>
+                        <p>
+                            <a href="Documentation/SmartLinx_API Reference.pdf"
+                                className="text-neutral-600 dark:text-neutral-200"
+                            >
+                                API Documentation
+                            </a>
+                        </p>
                     </div >
-        <div>
-            <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-                Contact
-            </h6>
-            <p className="mb-4 flex items-center justify-center md:justify-start">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="mr-3 h-5 w-5"
-                >
-                    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-                    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
-                </svg>
-                Crema, CR 26013, Italy
-            </p>
-            <p className="mb-4 flex items-center justify-center md:justify-start">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="mr-3 h-5 w-5"
-                >
-                    <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
-                    <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
-                </svg>
-                support@smartlinx.it
-            </p>
+                    <div>
+                        <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                            Contact
+                        </h6>
+                        <p className="mb-4 flex items-center justify-center md:justify-start">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="mr-3 h-5 w-5"
+                            >
+                                <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                                <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+                            </svg>
+                            Crema, CR 26013, Italy
+                        </p>
+                        <p className="mb-4 flex items-center justify-center md:justify-start">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="mr-3 h-5 w-5"
+                            >
+                                <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+                                <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+                            </svg>
+                            support@smartlinx.it
+                        </p>
 
-            <p className="flex items-center justify-center md:justify-start">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="mr-3 h-5 w-5"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.117a.75.75 0 01-.75.75h-.008a.75.75 0 01-.75-.75v-.117z"
-                        clipRule="evenodd"
-                    />
-                </svg>
-                Crema, CR 26013, Italy
-            </p>
-        </div>
+                        <p className="flex items-center justify-center md:justify-start">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="mr-3 h-5 w-5"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.117a.75.75 0 01-.75.75h-.008a.75.75 0 01-.75-.75v-.117z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            Crema, CR 26013, Italy
+                        </p>
+                    </div>
                 </div >
             </div >
         </footer >
     );
 }
 
-<<<<<<< HEAD
-=======
 
 
 
->>>>>>> master
 export default Footer;
