1cc85e60c869b0b5cfa5f453e98b8f1a47780b25
Nino Zullo <nino.zullo05dev@gmail.com>
2024-05-30 11:09:23 +0200

    fixed FullScreenMode

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,13 +1,11 @@
-
 import React, { useEffect, useState } from 'react';
+
 /**
  * A React functional component that renders a video element.
  *
  * @function Video
  * @returns {JSX.Element} - A JSX element representing the video component.
  */
-
-
 export default function Video() {
     const [loading, setLoading] = useState(true);
 
@@ -25,20 +23,22 @@ export default function Video() {
     };
 
     return (
-        <div id="video" className="pt-16" >
+        <div id="video" className="pt-16">
             <div className="flex flex-col items-center mx-4 sm:mx-12">
                 <h1 className="text-gray-900 dark:text-gray-100 text-3xl font-bold mb-10 mt-5 mx-5 text-center truncate">
                     Video of our application
                 </h1>
-                <div className={`rounded-lg m-auto w-full h-72 sm:w-2/3 md:w-1/2 lg:w-1/2 bg-gray-300 dark:bg-gray-700 ${loading ? 'animate-pulse' : ''}`} style={{ display: loading ? 'block' : 'none' }}></div>
+                {loading && (
+                    <div className="rounded-lg m-auto w-full h-72 sm:w-2/3 md:w-1/2 lg:w-1/2 bg-gray-300 dark:bg-gray-700 animate-pulse"></div>
+                )}
                 <iframe
                     src="https://drive.google.com/file/d/1s_tjnkgE_WG_Zuiqrui4HJ6uwxqiCbOU/preview"
                     allow="autoplay"
+                    allowFullScreen
                     className={`rounded-lg m-auto w-full h-auto sm:w-2/3 md:w-1/2 lg:w-1/2 ${loading ? 'hidden' : ''}`}
                     style={{ aspectRatio: '16/9' }}
                     title="Video of our application"
                     onLoad={handleVideoLoad}
-
                 ></iframe>
             </div>
         </div>
