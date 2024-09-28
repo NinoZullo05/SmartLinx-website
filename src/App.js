ad3e95e6fcc63cc340dbac71f36232b8423afc05
Nino Zullo <nino.zullo05dev@gmail.com>
2024-09-28 15:50:32 +0200

    fixed documentation

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import DefaultPage from './Pages/DefaultPage';
 import ErrorPage from './Pages/ErrorPage';
-
+import DocumentationPage from './Pages/DocumentationPage';
 function App () {
   return (
     <div className="bg-bg_light dark:bg-bg_dark  custom - scrollbar">
-
       <BrowserRouter>
         <Routes>
           <Route index element={<DefaultPage />} />
+          <Route path="documentation" element={<DocumentationPage />} />
+
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
